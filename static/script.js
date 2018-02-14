2d146d7c00f014bf2482ace00e1ba578b5d6e583
Alena <acrylami@gmail.com>
2018-02-14 18:44:53 +0000

    fasdf

--- static/script.js
+++ static/script.js
@@ -857,6 +857,7 @@ function displayPokemon(){
         });
         $("tbody tr").click(function() {
             var $this = $(this);
+            //sfd
             if ($this.hasClass("selected")) {
                 $this.attr("title", "Click to " + (isForIndividualPokemon ? "display more information and " : '') + "add to Reddit table.");
             } else {
